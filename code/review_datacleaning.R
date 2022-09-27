4746814f6eec113eb722372ecbd507fe46c036cd
Armin Fabian <armin.fabian@uni-tuebingen.de>
2022-09-27 14:26:31 +0200

    initial descriptive results

--- code/review_datacleaning.R
+++ code/review_datacleaning.R
@@ -2,10 +2,85 @@ library(tidyverse)
 library(dplyr)
 library(psych)
 library(rio)
+library(gmodels)
 
 
-# Daten einlesen
+
+
+# read data
 data <- rio::import("data\\tpack_review_new_16092022.xlsx")
+
+# deleting the last two rows of the data set
+data <- head(data, -2)
+
+# new variable factor_sample consisting of each sample group type (pre-/in service & teacher educators)
+data <- mutate(data, factor_sample = case_when(
+                                  data$PST == 1 ~ "pre_service",
+                                  data$'In-service' == 1 ~ "in_service",
+                                  data$`teacher educators` == 1 ~ "educators"))
+
+factor_sample <- as.factor(data$factor_sample)
+
+# new variable "factor_countries" consisting of each country
+data <- mutate(data, factor_country = case_when(data$Africa == 1 ~ "Africa",
+                                                data$Australie == 1 ~ "Australia",
+                                                data$`North-America` == 1 ~"North-America",
+                                                data$`South-America`== 1 ~"South-America",
+                                                data$Europe == 1 ~ "Europe",
+                                                data$Asia == 1 ~ "Asia"))
+
+factor_country <- as.factor(data$factor_country)
+
+# new variable "factor_method" consisting of each methodological type (quant, qual, mixed)
+data <- mutate(data, factor_method = case_when(data$mixed == 1 ~ "mixed",
+                                                data$quantitative == 1 ~ "quantitative",
+                                                data$qualitative == 1 ~"qualitative"))
+factor_method <- as.factor(data$factor_method)
+
+# DESCRIPTIVE RESULTS...
+#.... regarding countries
+print(table(data$factor_country))
+print(100*(prop.table(table(data$factor_country)))) # in percentage
+
+#... regarding sample groups
+print(table(data$factor_sample))
+print(100*(prop.table(table(data$factor_sample))))
+
+#... regarding method used
+print(table(data$factor_method))
+print(100*(prop.table(table(data$factor_method))))
+
+
+
+                                                
+
+
+##### descriptive 3-way contigency tables ######
+
+# Analyse; Abhängigkeit TPCK, PCK und sample groups (3-way contingency table, https://www.r-bloggers.com/2020/12/contingency-tables-in-r/)
+tab_1 <- xtabs(~data$PCK + data$TPCK + data$lvl_sample, data=data)
+
+#  Analyse; Abhängigkeit TPCK, TPK und sample groups
+tab_2 <- xtabs(~data$TPK + data$TPCK + data$lvl_sample, data=data)
+
+#  Analyse; Abhängigkeit TPCK, PCK and self-reports
+tab_3 <- xtabs(~data$PCK + data$TPCK + data$self_report, data=data)
+
+# Abhängigkeit PCK, Self-Reports
+tab_4 <- table(data$PCK, data$self_report)
+
+# PCK und Performance measurements
+tab_5 <- table(data$PCK, data$performance)
+
+# PCK und sample group
+tab_6 <- table(data$PCK, data$lvl_sample)
+
+
+
+
+
+# PCK variable as factor
+
 
 
 # Kreuztabellen (2x2 Kontingenztabellen) für alle Kombinationen der tpACK subdomains
@@ -33,7 +108,7 @@ loop_results <- data.frame(var1 = as.character(),
 for (var1i in names(data_loop)) {
   for (var2i in names(data_loop)) {
     kreuztabelle <- table(data[,var1i], data[,var2i])
-    
+
     loop_results <- loop_results %>%
       add_row(var1 = var1i,
               var2 = var2i,
@@ -42,6 +117,11 @@ for (var1i in names(data_loop)) {
 }
 
 print(loop_results)   #Korellationstabelle (Phi Korelation, siehe Eid et al., S. 555)
+
+
+
+
+
 
 # Verteilung nach Jahren
 barplot(table(data$Year), xlab = "year", ylab="numbers of interventions")
